45c884075707326a260f47afe4ad35c944600ff9
Maximilian Pal <max.pal@outlook.com>
2019-10-24 19:55:02 +0200

    Add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request
 from util import json_response
-import persistencepython
+import persistence
 import data_handler
 
 app = Flask(__name__)
